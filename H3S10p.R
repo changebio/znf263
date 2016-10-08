8e85e841aaa4ac0ca16bad0dc40e3aaef872b003
changebio <changebio@yeah.net>
2016-10-08 13:00:29 +0800

    no enrichment

--- H3S10p.R
+++ H3S10p.R
@@ -11,3 +11,11 @@ table(countOverlaps(h3s10p1,h3s10p2))
 #0    1    2 
 #7186 3865   16 
 # not good
+
+#TFs
+mm9.fs<-list.files("/mnt/local-disk1/rsgeno2/huangyin/RoadMap/proj1/mouse_mm9_TF_peaks/",pattern = "bed")
+mm9.tfs<- lapply(mm9.fs, function(x)import.bed(paste0("/mnt/local-disk1/rsgeno2/huangyin/RoadMap/proj1/mouse_mm9_TF_peaks/",x)))
+names(mm9.tfs)<- mm9.fs
+
+sapply(mm9.tfs,function(x)table(countOverlaps(x,h3s10p1)))
+#~no enrichment
